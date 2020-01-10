79b8a1bb6cdcef59374eb9f64cf13003eca4c6da
Mikael <test.kurs@hotmail.net>
2020-01-10 11:18:09 -0800

    Generated Identity Files (Index, Login, Register)

---
+++ Ovning17_Gym/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ovning17_Gym.Data;
+
+[assembly: HostingStartup(typeof(Ovning17_Gym.Areas.Identity.IdentityHostingStartup))]
+namespace Ovning17_Gym.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
