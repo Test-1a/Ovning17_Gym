fd8cd6a7dab02556bb42eddacd93bae4b2ea038a
Mikael <test.kurs@hotmail.net>
2020-01-10 13:35:21 -0800

    Added ApplicationUserGymClass class

---
+++ Ovning17_Gym/Models/ApplicationUserGymClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ovning17_Gym.Models
+{
+    public class ApplicationUserGymClass
+    {
+        public int Id { get; set; }
+        public int GymClassId { get; set; }
+        public int ApplicationUserId { get; set; }
+
+        public GymClass GymClass { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+
+    
+
+}
