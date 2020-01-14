d6471b156656fb48b7460d78dd7ce7cef3a0c368
Mikael <test.kurs@hotmail.net>
2020-01-14 14:12:46 -0800

    Creating roles in v0.4

---
+++ Ovning17_Gym/Data/SeedData.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Ovning17_Gym.Data;
+using Ovning17_Gym.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ovning17_Gym.Data
+{
+    public static class SeedData
+    {
+        internal static async Task InitializeAsync(IServiceProvider services, string adminPW)
+        {
+            var options = services.GetRequiredService<DbContextOptions<ApplicationDbContext>>();
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+
+                var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+                var roleNames = new[] { "Admin", "Member" };
+
+
+                foreach (var name in roleNames)
+                {
+                    //Om rollen finns fortsätt
+                    if (await roleManager.RoleExistsAsync(name)) continue;
+
+                    //Annars skapa rollen
+                    var role = new IdentityRole { Name = name };
+
+                    var result = await roleManager.CreateAsync(role);
+
+
+                    if (!result.Succeeded)
+                    {
+                        throw new Exception(string.Join("\n", result.Errors));
+                    }
+                }
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Ovning17_Gym/Program.cs
+++ Ovning17_Gym/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Ovning17_Gym.Data;
 
 namespace Ovning17_Gym
 {
@@ -13,7 +16,34 @@ namespace Ovning17_Gym
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var context = services.GetRequiredService<ApplicationDbContext>();
+
+                context.Database.Migrate();
+
+                var config = host.Services.GetRequiredService<IConfiguration>();
+
+
+                //Behöver sättas via kommandotolken i projektkatalogen
+                // dotnet user-secrets set "Gym:AdminPW" "LexiconNC19!"
+
+                var adminPW = config["Gym:AdminPW"];
+
+                try
+                {
+                    SeedData.InitializeAsync(services, adminPW).Wait();
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
